d0300f925c8793f906e833955143783a36f70e31
MKarim93 <mohammadekarim14@gmail.com>
2018-11-27 20:51:35 -0500

    example comment

---
+++ src/week1HW/NameAndAddress.java
@@ -0,0 +1,15 @@
+package week1HW;
+
+public class NameAndAddress {
+
+	public static void main(String[] args) {
+		
+		String name = "Bond, James Bond.";
+		String address = "1600 Pennsylvania Ave NW, Washington DC.";
+		
+		System.out.println("Name:"+" "+name+" "+"Sometimes Karim, Mohammad Karim.");
+		System.out.println("Address:"+" "+address+" "+"It's the big white house, you can't miss it.");
+
+	}
+
+}
